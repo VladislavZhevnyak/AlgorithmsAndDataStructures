5a5fcfd7c5c18313a2b523af2f18e6ef554738d0
Vlad <zhevnyakvlig.1.23@gmail.com>
2024-08-03 16:49:17 +0300

    Update binary search

--- binarySearch/binarySearch.go
+++ binarySearch/binarySearch.go
@@ -4,23 +4,22 @@ import "fmt"
 
 func main() {
 	arr := []int{1, 3, 5, 7, 9, 12, 15, 18, 22, 24}
-	item := -5
-	index := binarySearch(arr, item)
+	item := 7
+	index := binarySearch(arr, 0, len(arr)-1, item)
 	fmt.Printf("Index of %d is %d\n", item, index)
 }
 
-func binarySearch(arr []int, item int) int {
-	low, high := 0, len(arr)-1
-	for low <= high {
-		mid := (low + high) / 2
-		value := arr[mid]
-		if item == value {
-			return mid
-		} else if value > item {
-			high = mid - 1
-		} else {
-			low = mid + 1
-		}
+func binarySearch(arr []int, left int, right int, item int) int {
+	if left > right {
+		return -1
 	}
-	return -1
+	middle := (left + right) / 2
+	if arr[middle] == item {
+		return middle
+	}
+	if item > arr[middle] {
+		return binarySearch(arr, middle+1, right, item)
+	} else {
+		return binarySearch(arr, left, middle-1, item)
+	}
 }
