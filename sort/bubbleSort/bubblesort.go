f1b830d0e86f1fae70ed16272e00e01459e3b5bb
Vlad <zhevnyakvlig.1.23@gmail.com>
2024-08-04 13:09:53 +0300

    Update bubble sort

---
+++ sort/bubbleSort/bubblesort.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func bubbleSort(arr []int) {
+	n := len(arr)
+	for i := 0; i < n; i++ {
+		for j := 0; j < n-i-1; j++ {
+			if arr[j] > arr[j+1] {
+				arr[j], arr[j+1] = arr[j+1], arr[j]
+			}
+		}
+	}
+}
+
+func main() {
+	arr := []int{64, 34, 25, 12, 22, 11, 5}
+	fmt.Println("Unsorted array:", arr)
+
+	bubbleSort(arr)
+
+	fmt.Println("Sorted array:", arr)
+}
