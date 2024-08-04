33a0114da8b35d2f8b88bb872fae534d9fc658be
Vlad <zhevnyakvlig.1.23@gmail.com>
2024-08-04 14:19:01 +0300

    add insertion sort

---
+++ sort/insertionSort/insertionSort.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{8, 5, 9, 0, 4, 1}
+	insertionSort(arr)
+	fmt.Println(arr)
+}
+
+func insertionSort(arr []int) {
+	for i := 1; i < len(arr); i++ {
+		key := arr[i]
+		j := i - 1
+		for j >= 0 && arr[j] > key {
+			arr[j+1] = arr[j]
+			j--
+		}
+		arr[j+1] = key
+	}
+}
+
