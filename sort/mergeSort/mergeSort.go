ca2e28d58767d00555427f90495bb9ae80770d3b
Vlad <zhevnyakvlig.1.23@gmail.com>
2024-09-14 15:42:04 +0300

    add some dir

---
+++ sort/mergeSort/mergeSort.go
@@ -0,0 +1,38 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := []int{5, 8, 10, 0, 5, 4}
+	newArr := mergeSort(arr)
+	fmt.Println(newArr)
+}
+
+func mergeSort(arr []int) []int {
+	if len(arr) < 2 {
+		return arr
+	}
+	mid := len(arr) / 2
+	left := mergeSort(arr[:mid])
+	right := mergeSort(arr[mid:])
+
+	return merge(left, right)
+}
+
+func merge(left []int, right []int) []int {
+	result := make([]int, 0, len(left)+len(right))
+	i, j := 0, 0
+
+	for i < len(left) && j < len(right) {
+		if left[i] < right[j] {
+			result = append(result, left[i])
+			i++
+		} else {
+			result = append(result, right[j])
+			j++
+		}
+	}
+	result = append(result, left[i:]...)
+	result = append(result, right[j:]...)
+	return result
+}
